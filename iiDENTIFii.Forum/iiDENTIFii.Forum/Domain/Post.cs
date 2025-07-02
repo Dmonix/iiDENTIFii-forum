6839f214d6f642f003ec4523ed284641d8ddb5b1
Donald Dunlop <donalddunlop1988@gmail.com>
2025-07-02 19:39:33 +0200

    feat: Added intial Domain objects

---
+++ iiDENTIFii.Forum/iiDENTIFii.Forum/Domain/Post.cs
@@ -0,0 +1,26 @@
+﻿namespace iiDENTIFii.Forum.Domain
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required User Author { get; set; }
+        public List<PostComment> Comments { get; set; } = new List<PostComment>();
+        public int Likes { get; set; }
+        public bool IsTagged { get; set; } = false;
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public bool IsModerator { get; set; } = false;
+    }
+
+    public class PostComment
+    {
+        public int Id { get; set; }
+        public required string Content { get; set; }
+    }
+}
