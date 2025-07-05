ff1e171eb4c02edea3567dc61600a06b14df7c8e
Donald Dunlop <donalddunlop1988@gmail.com>
2025-07-05 11:37:33 +0200

    feat: Added a number of test cases to cover core functionality

--- iiDENTIFii.Forum/iiDENTIFii.Forum.Tests/Services/PostServiceTests.cs
+++ iiDENTIFii.Forum/iiDENTIFii.Forum.Tests/Services/PostServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using iiDENTIFii.Forum.Interfaces;
+using iiDENTIFii.Forum.Models;
 using iiDENTIFii.Forum.Services;
 using Moq;
 
@@ -8,10 +9,37 @@ namespace iiDENTIFii.Forum.Tests.Services
     {
         private IPostService postService;
         private DatabaseContext databaseContext;
+        private User authorUser;
+        private User likingUser;
+        private User modUser;
 
         [SetUp]
         public void Setup()
         {
+            authorUser = new User()
+            {
+                Email = "arthur@gmail.com",
+                UserName = "arthur@gmail.com",
+                DisplayName = "Arthur",
+                IsModerator = false
+            };
+
+            likingUser = new User()
+            {
+                Email = "luke@gmail.com",
+                UserName = "luke@gmail.com",
+                DisplayName = "Luke",
+                IsModerator = false
+            };
+
+            modUser = new User()
+            {
+                Email = "martin@gmail.com",
+                UserName = "martin@gmail.com",
+                DisplayName = "Martin",
+                IsModerator = true
+            };
+
             databaseContext = new Mock<DatabaseContext>().Object;
             postService = new PostService(databaseContext);
         }
@@ -22,7 +50,7 @@ namespace iiDENTIFii.Forum.Tests.Services
             var posts = postService.GetPosts();
 
             Assert.IsNotNull(posts);
-            Assert.GreaterOrEqual(posts.Count, 0);
+            Assert.Greater(posts.Count, 0);
         }
 
         [Test]
@@ -32,6 +60,64 @@ namespace iiDENTIFii.Forum.Tests.Services
 
             Assert.IsNotNull(post);
             Assert.IsTrue(post.Comments.Count >= 0);
+        }
+
+        [Test]
+        public void CreatePost_ShouldPass()
+        {
+            var newPost = new Post()
+            {
+                Title = "Text",
+                Content = "Lorem Ipsum Dolor...",
+                Author = authorUser
+            };
+
+            var post = postService.AddPost(newPost);
+
+            Assert.IsNotNull(post);
+            Assert.Greater(post.Id, 0);
+        }
+
+        [Test]
+        public void LikePost_ShouldPass()
+        {
+            var post = postService.GetPost(1);
+
+            var result = postService.LikePost(post.Id, likingUser);
+
+            Assert.IsTrue(result.Item1);
+        }
+
+        [Test]
+        public void LikePost_ShouldFail()
+        {
+            var post = postService.GetPost(1);
+
+            var result = postService.LikePost(post.Id, authorUser);
+
+            Assert.IsFalse(result.Item1);
+            Assert.IsNotNull(result.Item2);
+            Assert.Greater(result.Item2.Length, 0);
+        }
+
+        [Test]
+        public void TagPost_ShouldPass()
+        {
+            var post = postService.GetPost(1);
+
+            postService.TagPost(post.Id, modUser);
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TagPost_ShouldFail()
+        {
+            var post = postService.GetPost(1);
+
+            postService.TagPost(post.Id, authorUser);
+
+            Assert.Pass();
         }
     }
 }
